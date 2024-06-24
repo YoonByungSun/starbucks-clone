a94494c073c584c947281adc9fd661c07d42215a
YoonByungSun <ssp31n@naver.com>
2024-06-24 22:26:45 +0900

    common module set

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-// 통합검색창 애니메이션
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
-
 // 배지 애니메이션
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -147,8 +128,3 @@ SpyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 });
-
-
-// 현재 연도
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
